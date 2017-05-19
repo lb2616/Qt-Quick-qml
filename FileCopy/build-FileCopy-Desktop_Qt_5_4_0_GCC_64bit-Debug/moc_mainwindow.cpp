cb247723db6ac52782f1b151f1dbcc4f06417ce6
lb2616 <1448372453@qq.com>
2017-05-19 20:38:03 +0800

    file copy

---
+++ FileCopy/build-FileCopy-Desktop_Qt_5_4_0_GCC_64bit-Debug/moc_mainwindow.cpp
@@ -0,0 +1,125 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.4.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../FileCopy/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.4.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[8];
+    char stringdata[104];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 11), // "copyStation"
+QT_MOC_LITERAL(2, 23, 0), // ""
+QT_MOC_LITERAL(3, 24, 4), // "flag"
+QT_MOC_LITERAL(4, 29, 6), // "qsText"
+QT_MOC_LITERAL(5, 36, 23), // "on_selectSrcBtn_clicked"
+QT_MOC_LITERAL(6, 60, 23), // "on_selectDesBtn_clicked"
+QT_MOC_LITERAL(7, 84, 19) // "on_startBtn_clicked"
+
+    },
+    "MainWindow\0copyStation\0\0flag\0qsText\0"
+    "on_selectSrcBtn_clicked\0on_selectDesBtn_clicked\0"
+    "on_startBtn_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       4,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    2,   34,    2, 0x08 /* Private */,
+       5,    0,   39,    2, 0x08 /* Private */,
+       6,    0,   40,    2, 0x08 /* Private */,
+       7,    0,   41,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::Int, QMetaType::QString,    3,    4,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        switch (_id) {
+        case 0: _t->copyStation((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2]))); break;
+        case 1: _t->on_selectSrcBtn_clicked(); break;
+        case 2: _t->on_selectDesBtn_clicked(); break;
+        case 3: _t->on_startBtn_clicked(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata))
+        return static_cast<void*>(const_cast< MainWindow*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 4)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 4;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 4)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 4;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
---
+++ FileCopy/build-FileCopy-Desktop_Qt_5_4_0_GCC_64bit-Debug/ui_mainwindow.h
@@ -0,0 +1,203 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.4.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QProgressBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QGridLayout *gridLayout;
+    QLabel *staLab;
+    QVBoxLayout *verticalLayout;
+    QHBoxLayout *horizontalLayout;
+    QLineEdit *srcLineEdit;
+    QPushButton *selectSrcBtn;
+    QHBoxLayout *horizontalLayout_2;
+    QLineEdit *desLineEdit;
+    QPushButton *selectDesBtn;
+    QPushButton *startBtn;
+    QHBoxLayout *horizontalLayout_3;
+    QLabel *label;
+    QProgressBar *totalProgressBar;
+    QLabel *label_2;
+    QProgressBar *sigProgressBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(794, 225);
+        QFont font;
+        font.setPointSize(6);
+        MainWindow->setFont(font);
+        MainWindow->setStyleSheet(QStringLiteral(""));
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        gridLayout = new QGridLayout(centralWidget);
+        gridLayout->setSpacing(6);
+        gridLayout->setContentsMargins(11, 11, 11, 11);
+        gridLayout->setObjectName(QStringLiteral("gridLayout"));
+        staLab = new QLabel(centralWidget);
+        staLab->setObjectName(QStringLiteral("staLab"));
+        QFont font1;
+        font1.setPointSize(22);
+        font1.setBold(true);
+        font1.setWeight(75);
+        staLab->setFont(font1);
+        staLab->setStyleSheet(QStringLiteral("color: rgb(255, 0, 0);"));
+        staLab->setFrameShape(QFrame::StyledPanel);
+        staLab->setAlignment(Qt::AlignCenter);
+
+        gridLayout->addWidget(staLab, 0, 0, 1, 2);
+
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setSpacing(6);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        srcLineEdit = new QLineEdit(centralWidget);
+        srcLineEdit->setObjectName(QStringLiteral("srcLineEdit"));
+        QFont font2;
+        font2.setPointSize(12);
+        srcLineEdit->setFont(font2);
+        srcLineEdit->setStyleSheet(QStringLiteral("background-color: rgb(255, 255, 255);"));
+
+        horizontalLayout->addWidget(srcLineEdit);
+
+        selectSrcBtn = new QPushButton(centralWidget);
+        selectSrcBtn->setObjectName(QStringLiteral("selectSrcBtn"));
+        selectSrcBtn->setFont(font2);
+        selectSrcBtn->setStyleSheet(QStringLiteral(""));
+
+        horizontalLayout->addWidget(selectSrcBtn);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setSpacing(6);
+        horizontalLayout_2->setObjectName(QStringLiteral("horizontalLayout_2"));
+        desLineEdit = new QLineEdit(centralWidget);
+        desLineEdit->setObjectName(QStringLiteral("desLineEdit"));
+        desLineEdit->setFont(font2);
+        desLineEdit->setStyleSheet(QStringLiteral("background-color: rgb(255, 255, 255);"));
+
+        horizontalLayout_2->addWidget(desLineEdit);
+
+        selectDesBtn = new QPushButton(centralWidget);
+        selectDesBtn->setObjectName(QStringLiteral("selectDesBtn"));
+        selectDesBtn->setFont(font2);
+        selectDesBtn->setStyleSheet(QStringLiteral(""));
+
+        horizontalLayout_2->addWidget(selectDesBtn);
+
+
+        verticalLayout->addLayout(horizontalLayout_2);
+
+
+        gridLayout->addLayout(verticalLayout, 2, 0, 1, 1);
+
+        startBtn = new QPushButton(centralWidget);
+        startBtn->setObjectName(QStringLiteral("startBtn"));
+        QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(startBtn->sizePolicy().hasHeightForWidth());
+        startBtn->setSizePolicy(sizePolicy);
+        QFont font3;
+        font3.setPointSize(18);
+        font3.setBold(true);
+        font3.setWeight(75);
+        startBtn->setFont(font3);
+        startBtn->setStyleSheet(QStringLiteral(""));
+
+        gridLayout->addWidget(startBtn, 2, 1, 1, 1);
+
+        horizontalLayout_3 = new QHBoxLayout();
+        horizontalLayout_3->setSpacing(6);
+        horizontalLayout_3->setObjectName(QStringLiteral("horizontalLayout_3"));
+        label = new QLabel(centralWidget);
+        label->setObjectName(QStringLiteral("label"));
+        label->setFont(font2);
+
+        horizontalLayout_3->addWidget(label);
+
+        totalProgressBar = new QProgressBar(centralWidget);
+        totalProgressBar->setObjectName(QStringLiteral("totalProgressBar"));
+        totalProgressBar->setMaximumSize(QSize(16777215, 16));
+        totalProgressBar->setFont(font2);
+        totalProgressBar->setValue(0);
+
+        horizontalLayout_3->addWidget(totalProgressBar);
+
+        label_2 = new QLabel(centralWidget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setFont(font2);
+
+        horizontalLayout_3->addWidget(label_2);
+
+        sigProgressBar = new QProgressBar(centralWidget);
+        sigProgressBar->setObjectName(QStringLiteral("sigProgressBar"));
+        sigProgressBar->setMaximumSize(QSize(16777215, 16));
+        sigProgressBar->setFont(font2);
+        sigProgressBar->setValue(0);
+
+        horizontalLayout_3->addWidget(sigProgressBar);
+
+
+        gridLayout->addLayout(horizontalLayout_3, 1, 0, 1, 2);
+
+        MainWindow->setCentralWidget(centralWidget);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "File Copy", 0));
+        staLab->setText(QApplication::translate("MainWindow", "\346\226\207\344\273\266\345\244\215\345\210\266", 0));
+        srcLineEdit->setText(QApplication::translate("MainWindow", "C:/Users/Lemon/Desktop/picture", 0));
+        selectSrcBtn->setText(QApplication::translate("MainWindow", " \351\200\211\346\213\251\346\226\207\344\273\266\345\244\271 ", 0));
+        desLineEdit->setText(QApplication::translate("MainWindow", "C:/Users/Lemon/Desktop/img", 0));
+        selectDesBtn->setText(QApplication::translate("MainWindow", " \347\233\256\346\240\207\346\226\207\344\273\266\345\244\271 ", 0));
+        startBtn->setText(QApplication::translate("MainWindow", "\345\274\200\345\247\213", 0));
+        label->setText(QApplication::translate("MainWindow", "\346\200\273\350\277\233\345\272\246\357\274\232", 0));
+        label_2->setText(QApplication::translate("MainWindow", "\345\215\225\350\277\233\345\272\246\357\274\232", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
